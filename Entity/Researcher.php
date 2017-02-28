e66979e3d25c9b8b586d74f03f908bf7b84b203b
escudem <mickael.escudero@crick.ac.uk>
2017-02-28 17:28:12 +0000

    Added entity Researcher. Not finished at all.

---
+++ Entity/Researcher.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Clarity\Entity;
+
+/**
+ * Description of Researcher
+ *
+ * @author Mickael Escudero
+ */
+class Researcher
+{
+    
+    protected $clarityId;
+    protected $clarityUri;
+    protected $email;
+    protected $firstName;
+    protected $labUri;
+    protected $lastName;
+    protected $phone;
+    protected $username;
+    
+}
