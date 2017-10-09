31d3cc106a2cacd72a31c8fda70c89fe922b4194
escudem <mickael.escudero@crick.ac.uk>
2017-10-09 17:11:36 +0100

    Started script to find sample info

---
+++ Scripts/sampleFinder.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Clarity\Scripts;
+
+require_once(__DIR__ . '/../autoload.php');
+
+use Clarity\Connector\ClarityApiConnector;
+use Clarity\EntityRepository\LabClarityRepository;
+use Clarity\EntityRepository\ResearcherClarityRepository;
+use Clarity\EntityRepository\ProjectClarityRepository;
+use Clarity\EntityRepository\SampleClarityRepository;
+
+/*
+ * FUNCTIONS
+ */
+
+function parseOptions(&$options, &$search, &$input, &$format, &$server)
+{
+    if (array_key_exists('sample-id', $options)) {
+        $input = 'sample-id';
+    } elseif (array_key_exists('sample-name', $options)) {
+        $input = 'sample-name';
+    } elseif (array_key_exists('project-id', $options)) {
+        $input = 'project-id';
+    } elseif (array_key_exists('project-name', $options)) {
+        $input = 'project-name';
+    } elseif (array_key_exists('fastq', $options)) {
+        $input = 'fastq';
+    }
+    $search = $options[$input];
+}
+
+function usage()
+{
+    $message = 'Usage: ' . PHP_EOL;
+    $message .= 'php sampleFinder.php --sample-id|--sample-name|--project-id|--project-name|--fastq <search value> [--format yaml] [--server <test or prod>]';
+    return $message;
+}
+
+/*
+ * MAIN
+ */
+
+$format = 'yaml';
+$server = 'test';
+$search = '';
+$input = 'sample-id';
+
+$shortopts = '';
+$longopts = array('sample-id:', 'sample-name:', 'project-id:', 'project-name:', 'format:', 'server:', 'fastq:');
+$options = getopt($shortopts, $longopts);
+
+parseOptions($options, $search, $input, $format, $server);
+
+if (empty($search)) {
+    echo 'No search value was provided' . PHP_EOL . usage() . PHP_EOL;
+    exit;
+}
